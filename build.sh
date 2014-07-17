9a4f78bf09b96c65eca02571ddee03d8effe50a9
Lee Brunjes <lee@housemark.co>
2014-07-17 16:17:55 -0400

    fixed versionig issue

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "building diesel from files.txt"
-echo "// Diesel built at "$(date +%c) > diesel.js
 VERSION="0.7"
+echo "building diesel from files.txt"
+echo "// Diesel built at "$(date +%c) > diesel-$VERSION.js
+
 
 cat files.txt| while read line; do
 	cat $line >> diesel-$VERSION.js
@@ -9,7 +10,7 @@ cat files.txt| while read line; do
 done
 echo "";
 
-echo "//diesel tests built at "$(date +%c) > tests.js
+echo "//diesel tests built at "$(date +%c) > tests-$VERSION.js
 for test in `ls tests/*.js`; do
 	cat $test >> tests-$VERSION.js
 	echo "added test $test"
@@ -18,4 +19,5 @@ done
 echo "Finished: "$(date +%c)
 
 
-#cp diesel-$VERSION.js ~/web-local/diesel.js
+cp diesel-$VERSION.js ~/web-local/diesel.js
+cp tests-$VERSION.js ~/web-local/tests.js
