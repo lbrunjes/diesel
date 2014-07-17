9d055bb039fe8a28285cd40a5f5432a8c3edfd5a
Lee Brunjes <lee@housemark.co>
2014-07-17 16:10:35 -0400

    pretty up some of the minor stuff

--- build.sh
+++ build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 echo "building diesel from files.txt"
 echo "// Diesel built at "$(date +%c) > diesel.js
-
+VERSION="0.7"
 
 cat files.txt| while read line; do
-	cat $line >> diesel.js
+	cat $line >> diesel-$VERSION.js
 	echo "added $line"
 done
 echo "";
 
 echo "//diesel tests built at "$(date +%c) > tests.js
 for test in `ls tests/*.js`; do
-	cat $test >> tests.js
+	cat $test >> tests-$VERSION.js
 	echo "added test $test"
 done
 
 echo "Finished: "$(date +%c)
 
-cp diesel.js ~/web-local/diesel.js
+
+#cp diesel-$VERSION.js ~/web-local/diesel.js
---
+++ readme.md
@@ -0,0 +1,9 @@
+ Diesel is a simple event based game engine for html 5 games.
+
+ to use it:
+ 1) include diesel.js
+ 2) setupa diesel game object
+ 3)run the diesel.start(game object) command 
+
+ The engine will start running the by emitting draw and update events from the game container, the game listens to those and should draw nd update as needed.
+
