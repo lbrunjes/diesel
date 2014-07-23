96efa94c271c4617127fe0dcdf9fcf1b4f0474b6
Lee Brunjes <lee@housemark.co>
2014-07-23 12:39:24 -0400

    tests

--- tests/math.js
+++ tests/math.js
@@ -1,16 +1,21 @@
 ///
-// tests for math/
+// tests for math;
 ///
 
 
-var x = diesel.math.clamp(-1, 0, 255);
-equal(x,0);
+//clamp
+equal(diesel.math.clamp(-1, 0, 255),0);
+equal(diesel.math.clamp(256, 0, 255),255);
+equal(diesel.math.clamp(17, 0, 255),17);
 
-x = diesel.math.clamp(256, 0, 255);
-equal(x,255);
+//degrees
+equal(diesel.math.degrees(Math.PI), 180);
+equal(diesel.math.degrees(0), 0);
+equal(diesel.math.degrees(Math.PI*2), 360);
 
-x = diesel.math.clamp(17, 0, 255);
-equal(x,17);
+//radians
+equal(diesel.math.radians(180), Math.PI);
+equal(diesel.math.radians(0), 0);
+equal(diesel.math.radians(360), Math.PI*2);
 
-//
 
